8a3b24f329eeea4e69e438925ce93acee76a0b6d
Alejandro Duque <alegaduq@gmail.com>
2025-08-16 11:48:19 +0100

    Testing Iteration 4

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,27 +1,28 @@
 import { useState } from "react"
 import diceEmpty from "../assets/images/dice-empty.png"
-import diceOne from "../assets/images/dice1.png"
-import diceTwo from "../assets/images/dice2.png"
-import diceThree from "../assets/images/dice3.png"
-import diceFour from "../assets/images/dice4.png"
-import diceFive from "../assets/images/dice5.png"
-import diceSix from "../assets/images/dice6.png"
+import dice1 from "../assets/images/dice1.png"
+import dice2 from "../assets/images/dice2.png"
+import dice3 from "../assets/images/dice3.png"
+import dice4 from "../assets/images/dice4.png"
+import dice5 from "../assets/images/dice5.png"
+import dice6 from "../assets/images/dice6.png"
 
 function Dice() {
+const diceImages[diceEmpty, dice1, dice2, dice3, dice4, dice5, dice6];
 const [randomDiceValue, setRandomDiceValue]=useState(diceEmpty)
 
 const handleDisplay = ()=>{
     setRandomDiceValue(diceEmpty);
     setTimeout(()=>{
-        const randomDice = Math.floor(Math.random()*6)+1
-        setRandomDiceValue(randomDice);
+        const randomDice = Math.floor(Math.random()*6)
+        setRandomDiceValue(diceImages[lrandomIndex]);
     },1000)
 }
 
   return (
     <div>
-<button onClick={randomDice}>Roll Dice</button>
-
+<button onClick={handleDisplay}>Roll Dice</button>
+<img src={randomDice}></img>
 
     </div>
   )
